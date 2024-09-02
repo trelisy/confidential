6a4d7ce782ddb60b0b3094e483ee8edf0438253d
trelisy <138360365+trelisy@users.noreply.github.com>
2024-09-02 15:24:56 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function validateForm() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+    const modal = document.getElementById('modal');
+    const modalText = document.getElementById('modal-text');
+    const modalContent = document.querySelector('.modal-content');
+
+    // List of valid usernames and passwords
+    const users = {
+        'alex': 'ani',
+        'ene': '1125',
+        'jufo': 'henaro21',
+        'shako': 'aptiaqiaversi69'
+    };
+
+    // Check if the entered username exists in the users object and if the password matches
+    if (users[username] && users[username] === password) {
+        modalText.textContent = 'Access Granted';
+        modal.style.display = 'flex';
+        modalContent.style.borderColor = '#00ff00'; // Green border for "Access Granted"
+        
+        setTimeout(() => {
+            if (username === 'alex') {
+                window.location.href = 'display.html'; // Redirect to welcome.html for "alex"
+            } else {
+                window.location.href = 'display.html'; // Redirect to display.html for other users
+            }
+        }, 2000); // Wait 2 seconds before redirecting
+        return false; // Prevent form submission
+    } else {
+        modalText.textContent = 'Access Denied';
+        modal.style.display = 'flex';
+        modalContent.style.borderColor = '#ff0000'; // Red border for "Access Denied"
+        setTimeout(() => {
+            modal.style.display = 'none';
+        }, 2000); // Hide modal after 2 seconds
+        return false; // Prevent form submission
+    }
+}
